2be61188f6f986e72394e589f78ed3bae09631b1
Anthony Reis <reistonyt@hotmail.com>
2024-04-30 12:53:54 -0500

    Added a load balancer for nextjs to test auto scale capabilities.

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -4,3 +4,5 @@ kubectl apply -f kafka-deployment.yaml;
 kubectl apply -f nextjs-deployment.yaml;
 kubectl apply -f nginx-deployment.yaml;
 kubectl apply -f producer-deployment.yaml;
+
+kubectl apply -f load-generator.yaml;
